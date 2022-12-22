0722354ea694614b6065fb1b07a48b6d13b08f31
Tsion Moges <tsionmoges25@gmail.com>
2022-12-22 22:25:28 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,26 +1,25 @@
+#!/usr/bin/python3
+"""Pascal"""
+
+
 def pascal_triangle(n):
-    if n < 0 : return
-    # n = 0
-    T = [[1]]
-    # loop for the next r = [1:n+1]
-    for R in range(1, height + 2):
-        # c == 0
-        N = [1]
-        # caculate [1:r) (exclude r itself)
-        for C in range(1, R):
-            a = T[R - 1][C - 1]
-            b = T[R - 1][C]
-            # c = a + b
-            N.append(a + b)
-        # c == r
-        N.append(1)
-        T.append(N)
-    # print each R, from [0, n+1], total row is n+2
-    for R in T:
-        # i want to a pyramid, but the last row will
-        # still have a single leading space
-        print ' ' * (n - len(R) + 2),
-        for C in R:
-            print C,
-        # new line
-        print
+    """fucction to get the number of pascal triangle"""
+    if n <= 0:
+        return []
+
+    pascal_triangle = [0] * n
+
+    for i in range(n):
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
+        for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
