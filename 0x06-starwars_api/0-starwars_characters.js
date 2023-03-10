b3adacaa70fd6adae836d708fb5f258926c6d255
Tsion Moges <tsionmoges25@gmail.com>
2023-03-10 12:17:41 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
+const MyArgs = process.argv.slice(2);
 
-const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${MyArgs}`;
 
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-request(url, async (err, res, body) => {
-  err && console.log(err);
-
-  const charactersArray = (JSON.parse(res.body).characters);
-  for (const character of charactersArray) {
+request(url, async (error, response, body) => {
+  if (error) {
+    return console.log(error);
+  }
+  const res = JSON.parse(body).characters;
+  for (let i = 0; i < res.length; i++) {
     await new Promise((resolve, reject) => {
-      request(character, (err, res, body) => {
-        err && console.log(err);
-
-        console.log(JSON.parse(body).name);
+      request(res[i], (e, resp, bod) => {
+        if (e) {
+          reject(e);
+        }
+        console.log(JSON.parse(bod).name);
         resolve();
       });
     });
